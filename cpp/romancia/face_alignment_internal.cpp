f3f864027d78e1f759c5eb6845533fc06cc919b2
zhangyifu2016 <415699513@qq.com>
2021-11-29 12:04:16 +0800

    fix: include opencv2 types_c.h when compile android version

--- cpp/romancia/face_alignment_internal.cpp
+++ cpp/romancia/face_alignment_internal.cpp
@@ -13,6 +13,7 @@
 #include <Excalibur/operation_resize.hpp>
 #include <Excalibur/operation_rgb2gray.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/types_c.h>
 
 using glasssix::excalibur::rectangle;
 using glasssix::excalibur::point;
