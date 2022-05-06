6767fc2835230de0f27428be963e14bb6d74d562
renyuehe <601612274@qq.com>
2022-05-06 18:24:37 +0800

    delete yolovt

--- cpp/yolov5/exports.cpp
+++
@@ -1,5 +0,0 @@
-#include "yolov5m_net_impl.hpp"
-
-#include <abi/abi_standard_export.hpp>
-
-MAKE_ABI_STANDARD_EXPORT_FUNCTIONS(u8"g6.library.algorithmZoo.yolov5", glasssix::yolov5::yolov5m_net_impl)
