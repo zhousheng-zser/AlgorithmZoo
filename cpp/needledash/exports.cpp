14398581908b9c2a7fcb85d5b5a97f7aa82767c2
zhouzhou <zhouzhou@glasssix.com>
2023-07-26 17:01:58 +0800

    add: add needledash module into master banch and general_CV_SDK

---
+++ cpp/needledash/exports.cpp
@@ -0,0 +1,5 @@
+#include "ocr_code_impl.hpp"
+
+#include <abi/abi_standard_export.hpp>
+
+MAKE_ABI_STANDARD_EXPORT_FUNCTIONS(u8"g6.library.algorithmZoo.needledash", glasssix::needledash::ocr_code_impl)
