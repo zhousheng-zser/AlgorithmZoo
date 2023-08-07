663d91f74eccc2664d88aafdba4315a5278996bb
[username] <you@example.com>
2023-08-07 14:02:33 +0800

    chore: decoup hardcode of safeproduction

--- cpp/helmet/detect_code_internal.cpp
+++ cpp/helmet/detect_code_internal.cpp
@@ -22,7 +22,7 @@ namespace glasssix::helmet
     {
     public:
         impl(const exposing::param_string model_directory, int device = -1)
-                : impl{hardcode::get_model_params("helmet", false),  exposing::to_narrow_string(model_directory), device}
+                : impl{get_model_params("helmet", false),  exposing::to_narrow_string(model_directory), device}
         {
         }
 
