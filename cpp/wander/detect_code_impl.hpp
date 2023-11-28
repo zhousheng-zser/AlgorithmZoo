4d233b939957e51c41a517e02f82f1af71674b55
[username] <you@example.com>
2023-11-28 11:40:37 +0800

    modify: modify the wander judgement logic,add movement judgement to reduce false judge

--- cpp/wander/detect_code_impl.hpp
+++ cpp/wander/detect_code_impl.hpp
@@ -24,6 +24,7 @@ namespace glasssix::wander
         exposing::param_string version() const;
 
         exposing::param_string remove_library(std::int32_t device) const;
+        exposing::param_string remove_person_by_index(std::int32_t device_id, std::int32_t id)const;
 
         exposing::param_vector<wander::box_info> detect(exposing::param_span<std::uint8_t> bitmap, int channels, int height, int width, int roi_x, int roi_y, int roi_width, int roi_height,
            const exposing::param_hash_map<exposing::param_string, double>& param_map_abi) const;
--- cpp/wander/wander.hpp
+++ cpp/wander/wander.hpp
@@ -2,6 +2,7 @@
 #ifndef __WANDER_HPP__
 #define __WANDER_HPP__
 #include <map>
+#include <cmath>
 
     struct return_info
     {
@@ -15,6 +16,16 @@
         int y2;
     };
 
+
+    struct bbox
+    {
+        int id;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+    };
+
     struct wander_info
     {
         std::array<float,2048> feature;            
@@ -22,6 +33,22 @@
         double first_init_time;
         double last_match_time;
     };
+
+
+    float calculate_offset(bbox last_loca,bbox curr_loca) 
+    {        
+        int last_centre_x = (last_loca.x1 + last_loca.x2)/2  ;
+        int last_centre_y = (last_loca.y1 + last_loca.y2)/2  ;
+        int curr_centre_x = (curr_loca.x1 + curr_loca.x2)/2  ;
+        int curr_centre_y = (curr_loca.y1 + curr_loca.y2)/2  ;
+
+        // std::cout<< last_loca.x1<<" "<<last_loca.x2<<" "<<last_loca.y1<<" "<<last_loca.y2<<std::endl;
+        // std::cout<< curr_loca.x1<<" "<<curr_loca.x2<<" "<<curr_loca.y1<<" "<<curr_loca.y2<<std::endl;
+        // float width = curr_loca.x2 - curr_loca.x1;
+        float current_offset = sqrt(pow(curr_centre_x - last_centre_x , 2) + pow(curr_centre_y - last_centre_y , 2));
+        return current_offset;
+    }
+
 
     int get_id(std::map<int, wander_info> & feature_table,int num)//get a new allocate id
     {
@@ -68,9 +95,8 @@
         return xy/(sqrt_xx*sqrt_yy); 
     }
 
-     return_info feature_match(float *data,float sqrt_xx, double current_time, int devices,std::map<int, std::map<int, wander_info>>&  feature_tables, int table_size=200, float threshold=0.92)
+        return_info feature_match(float *data,float sqrt_xx, double current_time, int devices,std::map<int, std::map<int, wander_info>>&  feature_tables, std::vector<bbox>& last_loca, bbox& curr_bbox , int table_size=200, float threshold=0.92)
         { 
-
             return_info person_result;
             std::map<int, wander_info> feature_table;
             if(feature_tables.count(devices))
@@ -78,21 +104,68 @@
                 feature_table = feature_tables[devices];
             } 
 
+            float currend_box_width = curr_bbox.x2 - curr_bbox.x1;
+            float min_distance= 1000000000.f;
+
+            int id_distance =-1;
+            // std::cout<<"get last box: "<<last_loca.size()<<std::endl;
+            for (auto last_box: last_loca)
+            {
+                float tmp_min_distance = calculate_offset(last_box,curr_bbox );
+                // std::cout<<"tmp_min_distance match: "<<  tmp_min_distance <<std::endl;
+                if( min_distance > tmp_min_distance )
+                {
+                    min_distance = tmp_min_distance;
+                    id_distance = last_box.id;
+
+                    //
+
+                    
+                    // std::cout<<"location match: "<<  id_distance <<std::endl;
+                }
+            }
+            
             std::map<int, wander_info> ::iterator it;
+            float similiar =0.f;
+            int id=-1;
             for(it=feature_table.begin(); it != feature_table.end();  it++ )
             {   
                 float *data2 = feature_table[it->first].feature.data();
-                auto similiar = cosine_similiar(data, data2, sqrt_xx,feature_table[it->first].feature[2048] );
-                if(similiar>threshold)
+                auto tmp_similiar = cosine_similiar(data, data2, sqrt_xx,feature_table[it->first].feature[2048] );
+                if(similiar < tmp_similiar )
                 {
-                    feature_table[it->first].last_match_time = current_time;
-                    person_result.id = it->first;
-                    person_result.first_show_time = feature_table[it->first].first_init_time;
-                    person_result.last_show_time  = current_time;
-                    person_result.cosine_similarity = similiar;
-                    return person_result;
+                    // std::cout<<"tmp_similiar: "<<tmp_similiar<<std::endl;
+                    similiar = tmp_similiar;
+                    id = it->first;
                 }
             }
+
+            if( min_distance <currend_box_width )
+            {
+                feature_table[id_distance].last_match_time = current_time;
+                person_result.first_show_time = feature_table[id_distance].first_init_time;
+                person_result.last_show_time  = current_time;
+//获取特征值并添加
+                person_result.id = id_distance;
+                person_result.cosine_similarity = similiar;
+                // std::cout<<"location \n"<<id_distance<<std::endl;
+                return person_result;
+            }
+
+            if(similiar>threshold)
+            {
+                // std::cout<<"threshold: "<<threshold<<std::endl;
+                feature_table[id].last_match_time = current_time;
+                person_result.first_show_time = feature_table[id].first_init_time;
+                person_result.last_show_time  = current_time;
+//获取特征值并添加
+                person_result.id = id;
+                person_result.cosine_similarity = similiar;
+                // std::cout<<"simi\n"<<id<<std::endl;
+                return person_result;
+            }
+
+      
 
             {
                 auto id = get_id(feature_table, table_size);
