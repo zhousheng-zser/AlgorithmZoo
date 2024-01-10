ac5393fbc76836b27470af5fa6bbe09d2550d2b6
杨汉城 <hanchengyang@glasssix.com>
2024-01-10 16:39:48 +0800

    init: new Vehicle detect plugin

---
+++ cpp/vehicle/exports.cpp
@@ -0,0 +1,5 @@
+#include "classify_code_impl.hpp"
+
+#include <abi/abi_standard_export.hpp>
+
+MAKE_ABI_STANDARD_EXPORT_FUNCTIONS(u8"g6.library.algorithmZoo.vehicle", glasssix::vehicle::classify_code_impl)
