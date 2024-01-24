7422220833f82198593cc92c3acb84e4f6292691
jianzhang <zhangjian@glasssix.com>
2024-01-24 15:47:47 +0800

    Revert "refresh: replace head model and modify the output names"
    This reverts commit 73b2eb60619847d7ca992ce1539fcc897c7862e5.

--- cpp/head/detect_code_internal.cpp
+++ cpp/head/detect_code_internal.cpp
@@ -45,7 +45,7 @@ namespace glasssix::head
 
 std::string version()
         {
-			const std::string algo_module_version = "1.0.1";
+			const std::string algo_module_version = "1.1.0";
 
 #if defined(USE_RKNNAPI) || defined(USE_RKNN2API)
 			std::string nn_frame_version = net_detect_->version();
@@ -95,7 +95,7 @@ std::string version()
                 temp_result.x2=head_rect.x2;
                 temp_result.y2=head_rect.y2;
                 temp_result.score=body[4];
-                temp_result.key_points = exposing::make_param_vector<float>();
+                //temp_result.key_points = exposing::make_param_vector<float>();
            
                 result.push_back( temp_result  );
             }
@@ -167,6 +167,7 @@ std::string version()
                     posture_mul_weight_1280[i] = 32.f;
                 }
             }
+            
         }
 
         std::tuple<cv::Mat, float> preprocess_detection(cv::Mat& src,int& pad_h,int& pad_w,  cv::Size input_shape = cv::Size(640, 640) )
