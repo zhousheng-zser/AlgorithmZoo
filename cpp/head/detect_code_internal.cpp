b7529bdfd320d7278564dab9a134302b8eef92fc
[username] <you@example.com>
2024-01-25 10:55:49 +0800

    optimize: optimize the concat to accelerate the func

--- cpp/head/detect_code_internal.cpp
+++ cpp/head/detect_code_internal.cpp
@@ -276,17 +276,14 @@ std::string version()
                     match_index.push_back(i+ stride_8_num*stride_8_num + stride_16_num*stride_16_num );
 
             //concat the 80*40 40*40 20*20 
-            std::vector<float> cat(65*candidate_num);//1*65*candidate_num = 64*candidate_num + 1*candidate_num
+            std::vector<float> cat(65*candidate_num);//1*65*candidate_num = 64*candidate_num + 1*candidate_num        
             for(int i=0,j=0;i<65;i++,j=0)
             {   
-                for(; j<stride_8_num*stride_8_num; j++)             
-                    cat[ i*candidate_num + j] = data_stride_8[i*stride_8_num*stride_8_num + j];
-                for(; j<stride_16_num*stride_16_num+stride_8_num*stride_8_num; j++)              
-                    cat[ i*candidate_num + j] = data_stride_16[i*stride_16_num*stride_16_num + j- stride_8_num*stride_8_num];                        
-                for(; j<stride_32_num*stride_32_num+stride_16_num*stride_16_num+stride_8_num*stride_8_num; j++)
-                    cat[ i*candidate_num + j] = data_stride_32[i*stride_32_num*stride_32_num + j-stride_8_num*stride_8_num  -stride_16_num*stride_16_num ];
+                std::copy(data_stride_8+i*stride_8_num*stride_8_num, data_stride_8+(i+1)*stride_8_num*stride_8_num, cat.data()+i*candidate_num ); 
+                std::copy(data_stride_16+i*stride_16_num*stride_16_num, data_stride_16+(i+1)*stride_16_num*stride_16_num, cat.data()+i*candidate_num+stride_8_num*stride_8_num ); 
+                std::copy(data_stride_32+i*stride_32_num*stride_32_num, data_stride_32+(i+1)*stride_32_num*stride_32_num, cat.data()+i*candidate_num+stride_8_num*stride_8_num+stride_16_num*stride_16_num ); 
             }
-
+            
             //tranpose and softmax
             std::vector<float> reshape_box(candidate_num*64);
             tranpose(cat.data(),reshape_box.data(),64,candidate_num );
