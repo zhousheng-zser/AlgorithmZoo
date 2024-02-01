a477397667b4ae2b7d6b319a3c2b8f653813ff53
[username] <you@example.com>
2024-02-01 11:37:24 +0800

    refactor: refactor cassius,replace the 512dim model(which comes zhouyangrui int16)

--- cpp/cassius/feature_extractor_internal.cpp
+++ cpp/cassius/feature_extractor_internal.cpp
@@ -22,8 +22,8 @@ namespace glasssix::cassius
     namespace
     {
         constexpr std::size_t feature_size = 512;
-        constexpr std::size_t single_bitmap_width = 128;
-        constexpr std::size_t single_bitmap_height = 128;
+        constexpr std::size_t single_bitmap_width = 96;
+        constexpr std::size_t single_bitmap_height = 96;
         constexpr std::size_t single_bitmap_channels = 3;
         constexpr std::size_t single_bitmap_bytes = single_bitmap_channels * single_bitmap_width * single_bitmap_height;
     }
@@ -31,11 +31,12 @@ namespace glasssix::cassius
     class feature_extractor_internal::impl
     {
     public:
-        impl(std::int32_t model_type, std::string_view racy_path, int device, bool use_int8) : impl{get_model_params(model_type ? "unicorn_res101" : "unicorn", use_int8), racy_path, device}
+        impl(std::int32_t model_type, std::string_view racy_path, int device, bool use_int8) :
+        impl{get_model_params(model_type ? "SimpleRepUnicorn" : "SimpleRepUnicorn", use_int8), racy_path, device}
         {
         }
 
-        impl(const std::vector<std::string> &phai, std::string_view racy_path, int device) try : device_{device}, unicorn_{phai, std::string{racy_path}, device}
+        impl(const std::vector<std::string> &phai, std::string_view racy_path, int device) try : device_{device}, unicorn_{phai, std::string{racy_path}+std::string("/SimpleRepUnicorn96-N-part-deploy.rknn"), device}
         {
         }
         catch (...)
@@ -45,7 +46,7 @@ namespace glasssix::cassius
 
         std::vector<std::vector<float>> get(exposing::param_span<std::uint8_t> bitmaps, std::size_t count, int order)
         {
-            if (bitmaps.empty() || count <= 0)
+            if (bitmaps.empty())
             {
                 return {};
             }
@@ -53,36 +54,17 @@ namespace glasssix::cassius
             std::vector<std::vector<float>> result;
 #if defined(USE_RKNNAPI) || defined(USE_RKNN2API)
 #ifdef USE_RKNNAPI
-            auto network_result = unicorn_.forward(bitmaps.data(), { static_cast<int>(count), 3, 128, 128 }, static_cast<rknn_tensor_format>(order));
-            if (auto iter = network_result.find("conv5_dw_83_84"); iter != network_result.end())
+            auto network_result = unicorn_.forward(bitmaps.data(), { static_cast<int>(count), 96, 96, 3 }, static_cast<rknn_tensor_format>(order));
+            if (auto iter = network_result.find("Conv_Conv_258/out0_0"); iter != network_result.end())
 #else
             std::unordered_map<std::string, std::shared_ptr<memory::tensor<float>>> network_result;
-            if (order == 0)
-            {
-                std::vector<std::uint8_t> nhwc_bitmaps(count * 3 * 128 * 128);
-                for (size_t i = 0; i < count; i++)
-                {
-                    std::uint8_t* b = bitmaps.data() + i * 3 * 128 * 128;
-                    std::uint8_t* g = bitmaps.data() + i * 3 * 128 * 128 + 128 * 128;
-                    std::uint8_t* r = bitmaps.data() + i * 3 * 128 * 128 + 2 * 128 * 128;
-                    for (size_t j = 0; j < 128 * 128; j++)
-                    {
-                        *(nhwc_bitmaps.data() + i * 3 * 128 * 128 + j) = *(b + j);
-                        *(nhwc_bitmaps.data() + i * 3 * 128 * 128 + j + 1) = *(g + j);
-                        *(nhwc_bitmaps.data() + i * 3 * 128 * 128 + j + 2) = *(r + j);
-                    }
-                }
-                network_result = unicorn_.forward(nhwc_bitmaps.data(), { static_cast<int>(count), 3, 128, 128 }, rknn_tensor_format::RKNN_TENSOR_NHWC);
-            }
-            else
-                network_result = unicorn_.forward(bitmaps.data(), { static_cast<int>(count), 3, 128, 128 }, rknn_tensor_format::RKNN_TENSOR_NHWC);
-
-            if (auto iter = network_result.find("conv5_dw"); iter != network_result.end())
+            network_result = unicorn_.forward(bitmaps.data(), { static_cast<int>(count), 96, 96, 3 }, rknn_tensor_format::RKNN_TENSOR_NHWC);
+            if (auto iter = network_result.find("predict"); iter != network_result.end())
 #endif
 #else
             init_cache(bitmaps, count, order);
             auto network_result = unicorn_.forward(cache_ | memory::tensor_convert_to<float>);
-            if (auto iter = network_result.find("conv5_dw"); iter != network_result.end())
+            if (auto iter = network_result.find("predict"); iter != network_result.end())
 #endif
             {
                 auto iter_conv5 = iter->second->cpu_data();
@@ -90,13 +72,11 @@ namespace glasssix::cassius
                 for (std::size_t i = 0; i < count; i++)
                 {
                     std::vector<float> feature(feature_size);
-
                     std::copy(iter_conv5, iter_conv5 + feature_size, feature.data());
                     iter_conv5 += feature_size;
                     result.emplace_back(feature);
                 }
             }
-
             return result;
         }
 
@@ -135,7 +115,8 @@ namespace glasssix::cassius
         std::shared_ptr<memory::tensor<std::uint8_t>> cache_;
     };
 
-    feature_extractor_internal::feature_extractor_internal(std::int32_t model_type, std::string_view racy_path, int device, bool use_int8) : impl_{std::make_unique<impl>(model_type, racy_path, device, use_int8)}
+    feature_extractor_internal::feature_extractor_internal(std::int32_t model_type, std::string_view racy_path, int device, bool use_int8) : 
+    impl_{std::make_unique<impl>(model_type, racy_path, device, use_int8)}
     {
     }
 
