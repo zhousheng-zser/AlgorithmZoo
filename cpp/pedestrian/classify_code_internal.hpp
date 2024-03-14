8538f52b8ce3090415ce36bd6607da393f04a9cd
杨汉城 <hanchengyang@glasssix.com>
2024-03-14 16:14:36 +0800

    refactor: pedestrian apply GenPipeline and h734*w1280 model

--- cpp/pedestrian/classify_code_internal.hpp
+++ cpp/pedestrian/classify_code_internal.hpp
@@ -10,7 +10,7 @@
 #include <abi/param_span.hpp>
 #include <opencv2/opencv.hpp>
 #include "box_info.hpp"
-
+#include <GenPipeline/GenPipeTools.hpp>
 namespace glasssix::pedestrian
 {
     struct box_info_internal
@@ -23,60 +23,10 @@ namespace glasssix::pedestrian
         int category;
     };
 
-    struct Bbox {
-        float xmin;
-        float ymin;
-        float xmax;
-        float ymax;
-        float score;
-        int cid = 0;
-
-        void add(cv::Point2f point) {
-            xmin += point.x;
-            ymin += point.y;
-            xmax += point.x;
-            ymax += point.y;
-        }
-        void add(int x, int y) {
-            xmin += x;
-            ymin += y;
-            xmax += x;
-            ymax += y;
-        }
-
-        void mul_ratio(float ratio) {
-            xmin = xmin * ratio;
-            ymin = ymin * ratio;
-            xmax = xmax * ratio;
-            ymax = ymax * ratio;
-        }
-
-        std::vector<cv::Point2f> points() {
-            std::vector<cv::Point2f> rect_points{
-                cv::Point2f(std::round(xmin),std::round(ymin)),
-                cv::Point2f(std::round(xmin),std::round(ymax)),
-                cv::Point2f(std::round(xmax),std::round(ymin)),
-                cv::Point2f(std::round(xmax),std::round(ymax)) };
-            return rect_points;
-        }
-
-        cv::Rect get_rect() {
-            return cv::Rect{
-                cv::Point(std::round(xmin), std::round(ymin)),
-                cv::Point(std::round(xmax), std::round(ymax)) };
-        }
-
-        cv::Point2f get_center() {
-            auto p1 = cv::Point2f(std::round(xmin), std::round(ymin));
-            auto p2 = cv::Point2f(std::round(xmax), std::round(ymax));
-            return (p1 + p2) / 2;
-        }
-
-        float get_area() {
-            return (xmax - xmin) * (ymax - ymin);
-        }
+    struct PersonBBox :public GenPipTools::YoloBoxBase {
+    public:
+        using YoloBoxBase::YoloBoxBase; //Inheriting Constructors
     };
-
 
     class classify_code_internal
     {
