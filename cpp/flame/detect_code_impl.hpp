af2e97a17e286abcd1eaa752e4fb0b5310e83abf
杨汉城 <hanchengyang@glasssix.com>
2024-05-15 11:24:12 +0800

    refactor: flame protoc inside

--- cpp/flame/detect_code_impl.hpp
+++ cpp/flame/detect_code_impl.hpp
@@ -18,10 +18,10 @@ namespace glasssix::flame
         detect_code_impl();
         ~detect_code_impl();
 
-        void init(const exposing::param_string& model_directory, std::int32_t device);
+        void init(const exposing::param_string& str_params);
+        exposing::param_string execute(const exposing::param_hash_map<exposing::param_string, exposing::unknown_object>& input_params_map);
 
         exposing::param_string version() const;
-
         exposing::param_vector<flame::box_info> detect(exposing::param_span<std::uint8_t> bitmap, int channels, int height, int width, int roi_x, int roi_y, int roi_width, int roi_height,
            const exposing::param_hash_map<exposing::param_string, float>& param_map_abi) const;
 
