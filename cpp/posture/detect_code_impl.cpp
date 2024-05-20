9d433da33034ba46839b44f03efb00544b0cde51
jianzhang <zhangjian@glasssix.com>
2024-05-20 14:31:51 +0800

    算法拆分：姿态

--- cpp/posture/detect_code_impl.cpp
+++ cpp/posture/detect_code_impl.cpp
@@ -3,6 +3,8 @@
 #include "box_info_impl.hpp"
 #include <map>
 #include <utility>
+#include "json.h"
+#include <iostream>
 
 namespace glasssix::posture
 {
@@ -10,17 +12,89 @@ namespace glasssix::posture
 
 	detect_code_impl::~detect_code_impl() = default;
 
-
-	void detect_code_impl::init(const exposing::param_string& model_directory, std::int32_t device,std::int32_t model_type)
+	void detect_code_impl::init(const exposing::param_string & str_params)
 	{
-		impl_ = std::make_unique<detect_code_internal>(exposing::to_narrow_string(model_directory), device, model_type);
+		Json::Reader reader(Json::Features::strictMode());
+		Json::Value root;
+		if (!reader.parse(exposing::to_narrow_string(str_params), root))
+			throw Json::Exception("parse json failed");
+		std::string model_directory = root["models_directory"].asString();
+		int device = root.get("device", Json::Int(-1)).asInt();
+		impl_ = std::make_unique<detect_code_internal>(model_directory, device);
 	}
-
 	exposing::param_string detect_code_impl::version() const
 	{
 		return exposing::to_param_string(impl_->version());
 	}
+	exposing::param_string detect_code_impl::execute(const exposing::param_hash_map<exposing::param_string, exposing::unknown_object>& input_params_map) {
+		if (!impl_)
+			throw exposing::abi_invalid_operation(u8"selene internal object not initialized");
 
+		Json::Reader reader(Json::Features::strictMode());
+		Json::FastWriter writer;
+		Json::Value root, value;
+		if (!reader.parse(exposing::to_narrow_string(exposing::unbox<exposing::param_string>(input_params_map.get_value("params"))), root))
+			throw Json::Exception("parse json failed");
+		//$
+		std::cout << root.toStyledString() << std::endl;
+		Json::Value params = root.get("dyparams", Json::Value());
+
+		std::map<std::string, float> dynamic_param_map;
+
+		for (auto& param_name : params.getMemberNames()) {
+			dynamic_param_map.try_emplace(param_name, params[param_name].asFloat());
+		}
+
+		auto input_data = exposing::unbox<exposing::param_span<std::uint8_t>>(input_params_map.get_value("input_data"));
+		auto output_data = exposing::unbox<exposing::param_span<std::uint8_t>>(input_params_map.get_value("output_data"));
+		int order = exposing::unbox<int>(input_params_map.get_value("order"));
+		auto data_shape = input_params_map.get_value("data_shape").as<exposing::param_vector<int>>();
+
+
+		int num = data_shape[0];
+		int channels = data_shape[1];
+		int height = data_shape[2];
+		int width = data_shape[3];
+
+
+		int roi_x = 0;
+		int roi_y = 0;
+		int roi_width = width;
+		int roi_height = height;
+
+		auto result = impl_->detect(std::move(input_data), channels, height, width, roi_x, roi_y, roi_width, roi_height, dynamic_param_map);
+		Json::Value jarray_fire_detected(Json::arrayValue);
+		Json::Value jarray_boxes;
+		for (auto box : result)
+		{
+			Json::Value jobj_box;
+			// location
+			Json::Value jarray_points;
+			jarray_points["x1"] = Json::Int(box.x1());
+			jarray_points["y1"] = Json::Int(box.y1());
+			jarray_points["x2"] = Json::Int(box.x2());
+			jarray_points["y2"] = Json::Int(box.y2());
+
+			jobj_box["location"] = jarray_points;
+			jobj_box["score"] = Json::Value(box.score());
+
+			Json::Value jarray_key_points = Json::Value(Json::arrayValue);
+			auto key_points = box.key_points();
+			for (size_t i = 0; i < (int)key_points.size() / 3; i++) {
+				Json::Value KPoint;
+				KPoint["x"] = Json::Int(key_points[i * 3]);
+				KPoint["y"] = Json::Int(key_points[i * 3 + 1]);
+				KPoint["point_score"] = Json::Value(key_points[i * 3 + 2]);
+				jarray_key_points.append(KPoint);
+			}
+
+			jobj_box["key_points"] = jarray_key_points;
+			jarray_boxes.append(jobj_box);
+		}
+
+		value["info_list"] = jarray_boxes;
+		return exposing::to_param_string(writer.write(value));
+	}
 	exposing::param_vector<posture::box_info> detect_code_impl::detect(exposing::param_span<std::uint8_t> bitmap, int channels,
 		int height, int width, int roi_x, int roi_y, int roi_width, int roi_height, const exposing::param_hash_map<exposing::param_string, float>& param_map_abi) const
 	{
