aa4124478192e5b737cb433fb9119b4ac0e8e62d
zhousheng <zhousheng@glasssix.com>
2024-06-04 10:06:48 +0800

    restruct crossing 临时

---
+++ cpp/crossing/detect_code.hpp
@@ -0,0 +1,93 @@
+#ifndef _CROSSING_DETECT_CODE_HPP_
+#define _CROSSING_DETECT_CODE_HPP_
+
+#include "box_info.hpp"
+#include <abi/consumer.hpp>
+#include <algo_plugin_interface.hpp>
+
+namespace glasssix::crossing
+{
+    struct detect_code;
+}
+
+namespace glasssix::exposing::impl
+{
+    template <>
+    struct abi<crossing::detect_code>
+    {
+        using identity_type = type_identity_interface;
+
+        static constexpr guid id{ "1D5BC78A-93BF-4011-92A1-037688BB8013" };
+
+        struct type : abi_unknown_object
+        {
+            virtual std::int32_t G6_ABI_CALL init(abi_in_t<param_string> str_params) = 0;
+
+            virtual std::int32_t G6_ABI_CALL execute(abi_in_t<param_hash_map<param_string, unknown_object>> input_params_map, abi_out_t<param_string> result) = 0;
+
+            virtual std::int32_t G6_ABI_CALL version(abi_out_t<param_string> result) noexcept = 0;
+        };
+    };
+
+    template <typename Derived>
+    struct interface_vtable<Derived, crossing::detect_code> : interface_vtable_base<Derived, crossing::detect_code>
+    {
+
+        virtual std::int32_t G6_ABI_CALL init(abi_in_t<param_string> str_params) override
+        {
+            return abi_safe_call([&] { this->self().init(create_from_abi<param_string>(str_params)); });
+        }
+
+        virtual std::int32_t G6_ABI_CALL execute(abi_in_t<param_hash_map<param_string, unknown_object>> input_params_map, abi_out_t<param_string> result) override
+        {
+            return abi_safe_call([&] { *result = detach_abi(this->self().execute(create_from_abi<param_hash_map<param_string, unknown_object>>(input_params_map))); });
+        }
+
+        virtual std::int32_t G6_ABI_CALL version(abi_out_t<param_string> result) noexcept override
+        {
+            return abi_safe_call(
+                [&]
+                {
+                    *result = detach_abi(this->self().version());
+                }
+                );
+        }
+    };
+
+    template <>
+    struct abi_adapter<crossing::detect_code>
+    {
+        template <typename Derived>
+        struct type : enable_self_abi_awareness<Derived, crossing::detect_code>
+        {
+            void init(const param_string& str_params)
+            {
+                check_abi_result(this->self_abi().init(get_abi(str_params)));
+            }
+
+            param_string execute(const param_hash_map<param_string, unknown_object>& input_params_map)
+            {
+                param_string result{ nullptr };
+
+                return (check_abi_result(this->self_abi().execute(get_abi(input_params_map), put_abi(result))), result);
+            }
+
+            param_string version() const
+            {
+                param_string result{ nullptr };
+
+                return (check_abi_result(this->self_abi().version(put_abi(result))), result);
+            }
+        };
+    };
+}
+
+namespace glasssix::crossing
+{
+    struct detect_code : exposing::inherits<detect_code, exposing::nessus::algo_plugin_interface>
+    {
+        using inherits::inherits;
+    };
+}
+
+#endif
---
+++ cpp/crossing/detect_code_impl.hpp
@@ -0,0 +1,33 @@
+#ifndef __DETECT_CODE_IMPL_HPP__
+#define __DETECT_CODE_IMPL_HPP__
+
+#include "detect_code.hpp"
+
+#include <memory>
+#include <abi/consumer.hpp>
+
+namespace glasssix::crossing
+{
+    inline constexpr exposing::utf8_string_view crossing_detect_code_qualified_name{ u8"g6.crossing.detect_code" };
+
+    class detect_code_internal;
+
+    class detect_code_impl : public exposing::implements<detect_code_impl, detect_code>, public exposing::make_external_qualified_name<crossing_detect_code_qualified_name>
+    {
+    public:
+        detect_code_impl();
+        ~detect_code_impl();
+
+        void init(const exposing::param_string& str_params);
+
+        exposing::param_string version() const;
+
+        exposing::param_string execute(const exposing::param_hash_map<exposing::param_string, exposing::unknown_object>& input_params_map);
+
+    private:
+
+        std::unique_ptr<detect_code_internal> impl_;
+    };
+}
+
+#endif
