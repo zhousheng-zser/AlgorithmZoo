d9948764ebdf1f7a0c06584a8052c87cbdb076ae
jianzhang <zhangjian@glasssix.com>
2024-07-01 19:00:07 +0800

    攀爬修改内部detect逻辑;

--- cpp/climb_pedestrian/detect_code_internal.cpp
+++ cpp/climb_pedestrian/detect_code_internal.cpp
@@ -28,14 +28,13 @@ namespace glasssix::climb_pedestrian
         impl( std::string model_directory, int device)
         {
 #if defined(USE_RKNNAPI) || defined(USE_RKNN2API)
-            net_climb_ = std::make_shared<GenPipeline>(std::string(model_directory) + "/climbing.rknn", device);
+            net_climb_ = std::make_shared<GenPipeline>(std::string(model_directory) + "/climbing_pedestrian.rknn", device);
 #elif defined(USE_BMNN)
-            net_climb_ = std::make_shared<GenPipeline>(std::string(model_directory) + "/climbing.bmodel", device);
+            net_climb_ = std::make_shared<GenPipeline>(std::string(model_directory) + "/climbing_pedestrian.bmodel", device);
 #else
             net_climb_ = std::make_shared<GenPipeline>(get_model_params("climb_20240426cut"), std::string(model_directory) + "/climb_20240426cut.racy", device);
 #endif
-            net_climb_->manual_possible_normalization(std::array<float,3>{0.f,0.f,0.f},std::array<float,3>{1.f/255.f, 1.f/255.f, 1.f/255.f});
-            yolov8_instance = std::make_shared<Yolov8<GenPipeline,true, true>>(640, 384, net_climb_);
+            net_climb_->manual_possible_normalization(std::array<float,3>{104.f, 117.f, 123.f},std::array<float,3>{1.f/128.f, 1.f/128.f, 1.f/128.f});
         }
 
         exposing::param_vector<climb_pedestrian::box_info> detect(const exposing::param_span<std::uint8_t>& bitmap, int channels, int height, int width, int roi_x, int roi_y, int roi_width, int roi_height,  std::map<std::string, float>& param_map,const std::vector<PedestrianInfo> &pedestrain_info)
@@ -51,19 +50,32 @@ namespace glasssix::climb_pedestrian
             float con_thres = param_map.count("conf_thres") ? param_map["conf_thres"] : 0.5f;
             float nms_thres = param_map.count("nms_thres") ? param_map["nms_thres"] : 0.6f;
 
-            auto climb_objects = yolov8_instance->get_objects( image, con_thres, nms_thres );
-
+   //         auto climb_objects = yolov8_instance->get_objects( image, con_thres, nms_thres );
+   //         auto climb_objects = net_climb_->forward(image).begin()->second;
+   //         auto tensor_out_data = tensor_out->mutable_cpu_data();
+			//int index = std::max_element(result, result + 1) - result;
             auto results = exposing::make_param_vector<climb_pedestrian::box_info>();
             std::vector<climb_pedestrian::box_info_internal> boxs;
-            for (auto pinfo : climb_objects) 
+            for (auto pinfo : pedestrain_info) 
             {
                 climb_pedestrian::box_info_internal box;
-                box.x1=pinfo.x1;
-                box.y1=pinfo.y1;
-                box.x2=pinfo.x2;
-                box.y2=pinfo.y2;
-                box.confidence=pinfo.score;
-                box.category = pinfo.category ;
+                box.x1 = pinfo.x1;
+                box.y1 = pinfo.y1;
+                box.x2 = pinfo.x2;
+                box.y2 = pinfo.y2;
+                //int x1,x2,y1,y2;
+                //x1 = pinfo.x1;
+                //x2 = pinfo.x2;
+                //y1 = pinfo.y1;
+                //y2 = pinfo.y2;
+                cv::Mat body;
+                cv::Mat crop = image(cv::Range(box.x1, box.y1), cv::Range(box.x2, box.y2)).clone();
+                cv::cvtColor(crop, crop, cv::COLOR_BGR2RGB);
+                cv::resize(crop, body, cv::Size((int)(80), (int)80), cv::INTER_CUBIC);
+                auto climb_objects = net_climb_->forward(image).begin()->second->mutable_cpu_data();
+                int index = std::max_element(climb_objects, climb_objects + 1) - climb_objects;
+                box.confidence= climb_objects[index];
+                box.category = index ;
                 results.push_back(glasssix::exposing::make_as_first<box_info_impl>(box));
             }
             return results;
