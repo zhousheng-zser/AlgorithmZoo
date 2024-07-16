d0ab234ad53e51a237f46d6db2783d87d56d65cd
jianzhang <zhangjian@glasssix.com>
2024-07-16 11:55:58 +0800

    拆分攀爬跌倒算法

--- cpp/climb_tumble_pedestrian/detect_code_impl.cpp
+++ cpp/climb_tumble_pedestrian/detect_code_impl.cpp
@@ -3,7 +3,7 @@
 #include "box_info_impl.hpp"
 #include <map>
 #include <utility>
-#include "../posture/detect_code.hpp"
+#include "json.h"
 
 namespace glasssix::climb_tumble_pedestrian
 {
@@ -11,16 +11,124 @@ namespace glasssix::climb_tumble_pedestrian
 
 	detect_code_impl::~detect_code_impl() = default;
 
-
-	void detect_code_impl::init(const exposing::param_string& model_directory, std::int32_t device)
+	void detect_code_impl::init(const exposing::param_string& str_params)
 	{
-		impl_ = std::make_unique<detect_code_internal>(exposing::to_narrow_string(model_directory), device);
+		Json::Reader reader(Json::Features::strictMode());
+		Json::Value root;
+		if (!reader.parse(exposing::to_narrow_string(str_params), root))
+			throw Json::Exception("parse json failed");
+		std::string models_directory = root["models_directory"].asString();
+		int device = root.get("device", Json::Int(-1)).asInt();
+
+		impl_ = std::make_unique<detect_code_internal>(exposing::to_narrow_string(models_directory), device);
 	}
 
-	exposing::param_string detect_code_impl::version() const
+	exposing::param_string detect_code_impl::execute(const exposing::param_hash_map<exposing::param_string, exposing::unknown_object>& input_params_map)
 	{
-		return exposing::to_param_string(impl_->version());
+		if (!impl_)
+			throw exposing::abi_invalid_operation(u8"climb internal object not initialized");
+
+		Json::Reader reader(Json::Features::strictMode());
+		Json::FastWriter writer;
+		Json::Value root, value;
+		if (!reader.parse(exposing::to_narrow_string(exposing::unbox<exposing::param_string>(input_params_map.get_value("params"))), root))
+			throw Json::Exception("parse json failed");
+
+		Json::Value params = root.get("dyparams", Json::Value());
+
+		std::map<std::string, float> dparam_map;
+
+		for (auto& param_name : params.getMemberNames()) {
+			dparam_map.try_emplace(param_name, params[param_name].asFloat());
+		}
+
+		auto input_data = exposing::unbox<exposing::param_span<std::uint8_t>>(input_params_map.get_value("input_data"));
+		auto output_data = exposing::unbox<exposing::param_span<std::uint8_t>>(input_params_map.get_value("output_data"));
+		int order = exposing::unbox<int>(input_params_map.get_value("order"));
+		auto data_shape = input_params_map.get_value("data_shape").as<exposing::param_vector<int>>();
+		
+		int num = data_shape[0];
+		int channels = data_shape[1];
+		int height = data_shape[2];
+		int width = data_shape[3];
+
+		int roi_x = 0;
+		int roi_y = 0;
+		int roi_width = width;
+		int roi_height = height;
+		auto result = impl_->detect(std::move(input_data), channels, height, width, roi_x, roi_y, roi_width, roi_height, dparam_map);
+		Json::Value jarray_box;
+		Json::Value jarray_persion_detected(Json::arrayValue);
+		Json::Value jarray_climb_detected(Json::arrayValue);
+		Json::Value jarray_tumble_detected(Json::arrayValue);
+		Json::Value jarray_disabled_detected(Json::arrayValue);
+		Json::Value jarray_other_detected(Json::arrayValue);
+
+		for (int i = 0; i < result.size(); i++)
+		{
+			int category = Json::Int(result[i].category());
+			
+			if (category == 0)
+			{
+				jarray_box["x1"] = Json::Int(result[i].x1());
+				jarray_box["y1"] = Json::Int(result[i].y1());
+				jarray_box["x2"] = Json::Int(result[i].x2());
+				jarray_box["y2"] = Json::Int(result[i].y2());
+				jarray_box["score"] = Json::Value(result[i].confidence());
+				jarray_persion_detected.append(jarray_box);
+			}
+			else if (category == 1)
+			{
+				jarray_box["x1"] = Json::Int(result[i].x1());
+				jarray_box["y1"] = Json::Int(result[i].y1());
+				jarray_box["x2"] = Json::Int(result[i].x2());
+				jarray_box["y2"] = Json::Int(result[i].y2());
+				jarray_box["score"] = Json::Value(result[i].confidence());
+				jarray_climb_detected.append(jarray_box);
+			}
+			else if (category == 2)
+			{
+				jarray_box["x1"] = Json::Int(result[i].x1());
+				jarray_box["y1"] = Json::Int(result[i].y1());
+				jarray_box["x2"] = Json::Int(result[i].x2());
+				jarray_box["y2"] = Json::Int(result[i].y2());
+				jarray_box["score"] = Json::Value(result[i].confidence());
+				jarray_tumble_detected.append(jarray_box);
+			}
+			else if (category == 3)
+			{
+				jarray_box["x1"] = Json::Int(result[i].x1());
+				jarray_box["y1"] = Json::Int(result[i].y1());
+				jarray_box["x2"] = Json::Int(result[i].x2());
+				jarray_box["y2"] = Json::Int(result[i].y2());
+				jarray_box["score"] = Json::Value(result[i].confidence());
+				jarray_disabled_detected.append(jarray_box);
+			}
+			else if (category == 4)
+			{
+				jarray_box["x1"] = Json::Int(result[i].x1());
+				jarray_box["y1"] = Json::Int(result[i].y1());
+				jarray_box["x2"] = Json::Int(result[i].x2());
+				jarray_box["y2"] = Json::Int(result[i].y2());
+				jarray_box["score"] = Json::Value(result[i].confidence());
+				jarray_other_detected.append(jarray_box);
+			}
+		}
+
+		Json::Value jarray_info;
+
+		jarray_info["persion_list"] = jarray_persion_detected;
+		jarray_info["climb_list"] = jarray_climb_detected;
+		jarray_info["tumble_list"] = jarray_tumble_detected;
+		jarray_info["disabled_list"] = jarray_disabled_detected;
+		jarray_info["other_list"] = jarray_other_detected;
+
+		value["detect_info"] = jarray_info;
+
+		return exposing::to_param_string(writer.write(value));
 	}
+
+}
 
 	exposing::param_vector<climb_tumble_pedestrian::box_info> detect_code_impl::detect(exposing::param_span<std::uint8_t> bitmap, int channels, int height, int width,int roi_x, int roi_y, int roi_width, int roi_height,
 		const exposing::param_hash_map<exposing::param_string, float>& param_map_abi, 
