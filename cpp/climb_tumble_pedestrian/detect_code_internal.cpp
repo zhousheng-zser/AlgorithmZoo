f455c6718463137b622bdf2b853e4b42a00fece1
zhousheng <zhousheng@glasssix.com>
2024-07-17 14:41:59 +0800

    新增地铁异常检测

--- cpp/climb_tumble_pedestrian/detect_code_internal.cpp
+++ cpp/climb_tumble_pedestrian/detect_code_internal.cpp
@@ -171,13 +171,13 @@ namespace glasssix::climb_tumble_pedestrian
             std::shared_ptr<memory::tensor<float>> real_forwards;
 
             auto network_result = net_detect_light->forward(blob.data, { 1, blob.rows, blob.cols,blob.channels() }, RKNN_TENSOR_NHWC);
-            float* light_conf = network_result["output0"]->mutable_cpu_data();
+            float* cls_conf = network_result["output0"]->mutable_cpu_data();
             std::vector<float> current_frame_result;
-            current_frame_result.push_back(light_conf[0]);
-            current_frame_result.push_back(light_conf[1]);
-            current_frame_result.push_back(light_conf[2]);
-            current_frame_result.push_back(light_conf[3]);
-            current_frame_result.push_back(light_conf[4]);
+            current_frame_result.push_back(cls_conf[0]);
+            current_frame_result.push_back(cls_conf[1]);
+            current_frame_result.push_back(cls_conf[2]);
+            current_frame_result.push_back(cls_conf[3]);
+            current_frame_result.push_back(cls_conf[4]);
             return current_frame_result;
 
         }
