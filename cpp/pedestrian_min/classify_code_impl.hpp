06bdc8eb2f11af82959f5eb0a150556bc835cc31
zhousheng <zhousheng@glasssix.com>
2024-08-29 13:54:33 +0800

    行人迷你 OK

--- cpp/pedestrian_min/classify_code_impl.hpp
+++ cpp/pedestrian_min/classify_code_impl.hpp
@@ -8,7 +8,7 @@
 
 namespace glasssix::pedestrian_min
 {
-    inline constexpr exposing::utf8_string_view pedestrian_min_classify_code_qualified_name{ u8"g6.pedestrian_min.classify_code" };
+    inline constexpr exposing::utf8_string_view pedestrian_min_classify_code_qualified_name{ u8"g6.pedestrian_min.detect_code" };
 
     class classify_code_internal;
 
@@ -18,13 +18,11 @@ namespace glasssix::pedestrian_min
         classify_code_impl();
         ~classify_code_impl();
 
-        void init(const exposing::param_string& model_directory, std::int32_t device);
+        void init(const exposing::param_string& str_params);
 
         exposing::param_string version() const;
 
-        exposing::param_vector<pedestrian_min::box_info> detect(exposing::param_span<std::uint8_t> bitmap, int channels, int height, int width, int roi_x, int roi_y, int roi_width, int roi_height,
-            const exposing::param_hash_map<exposing::param_string, float>& param_map_abi) const;
-
+        exposing::param_string execute(const exposing::param_hash_map<exposing::param_string, exposing::unknown_object>& input_params_map);
     private:
 
         std::unique_ptr<classify_code_internal> impl_;
